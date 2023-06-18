263747f57c4af07e84060c02b3a942d043128d60
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-18 01:38:43 +0000

    Add Customers DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_18_010634) do
+ActiveRecord::Schema.define(version: 2023_06_18_012845) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,26 @@ ActiveRecord::Schema.define(version: 2023_06_18_010634) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.boolean "is_deleted", default: false, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
