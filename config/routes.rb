f2ff900a5d8818a75eb8411a13a84dcf1cd6e69c
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-06-19 02:37:05 +0000

    顧客、管理者登録画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  #管理者用
+  # URL /customers/sign_in...
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  #顧客用
+  devise_for :customers, skip: [:password], controllers: {
+    registarations: "public/registarations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
