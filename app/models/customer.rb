c5b258d69f26bc1cc011fdd0e85d9e7bc86681eb
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-20 06:39:54 +0000

    Add 会員登録&ログイン機能

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,10 +3,23 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :addresses, dependent: :destroy
+
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :email, presence: true, uniqueness: true
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+  validates :address, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
+
   
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy     
-  has_many :addresses, dependent: :destroy
+  
   
   # 氏名をまとめたメソッド
   def customer_name
