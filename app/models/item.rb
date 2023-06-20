402b2e9d0b57267d8943f2c569ac30942a58a62b
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-06-20 06:31:38 +0000

    注文関連の画面追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
  has_one_attached :image
-  has_many :carts, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
   
   # 消費税を含む金額
   def tax_price
