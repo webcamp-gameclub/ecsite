1f8b2031eb7d905b822df86e19467ca32f7bf837
uni1224 <saekahane99@gmail.com>
2023-06-20 01:12:31 +0000

    Add item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_19_074744) do
+ActiveRecord::Schema.define(version: 2023_06_18_090929) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2023_06_19_074744) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "addresses", force: :cascade do |t|
+  create_table "addres", force: :cascade do |t|
     t.integer "customer_id"
     t.string "name"
     t.string "address"
