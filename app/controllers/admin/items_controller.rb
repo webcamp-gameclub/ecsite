f621a3ca90d5b0e83321cf642f15b175878eff59
uni1224 <saekahane99@gmail.com>
2023-06-21 06:40:11 +0000

    Edited navi

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
     @items = Item.all
   end
@@ -14,7 +16,7 @@ class Admin::ItemsController < ApplicationController
   def create
   @item = Item.new(item_params)
    if @item.save
-     flash[:notice] = "登録が成功しました"
+     flash[:notice] = "商品を追加しました"
      redirect_to admin_item_path(@item)
    else
       render :new
@@ -28,7 +30,7 @@ class Admin::ItemsController < ApplicationController
   def update
      @item = Item.find(params[:id])
     if @item.update(item_params)
-      flash[:notice] = "編集が成功しました"
+      flash[:notice] = "商品を編集しました"
       redirect_to admin_item_path(@item)
     else
       render :edit
