c10babf10bd5272dc3a20afa706b52b7a052ae5c
Hikaru-q <132335482+Hikaru-q@users.noreply.github.com>
2023-06-21 08:07:02 +0000

    bug fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
  has_one_attached :image
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
+  belongs_to :genre
   validates :name, presence: true
   validates :description, presence: true
   validates :image, presence: true
