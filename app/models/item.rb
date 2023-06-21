6b67d66ce46b5de73bc9fb3a187d83bb1948a3b9
uni1224 <saekahane99@gmail.com>
2023-06-21 01:04:37 +0000

    注文履歴追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,11 @@ class Item < ApplicationRecord
  has_one_attached :image
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+  #validates :genre_id, presence: true
+  validates :price, presence: true
   
   # 消費税を含む金額
   def tax_price
