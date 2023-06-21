44a73a57acf5fe2f454c83355ec6a4e4ae1485e1
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-21 03:28:55 +0000

    Add　マイページ&登録情報更新機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -26,7 +26,7 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   def after_sign_in_path_for(resource)
-    root_path
+    mypage_path
   end
 
   def after_sign_out_path_for(resource)
