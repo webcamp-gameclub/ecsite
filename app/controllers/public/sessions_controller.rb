ca77629c39420d51fcd48ae1fc341f8853b8aa9a
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-22 01:49:47 +0000

    Add 退会機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :reject_inactive_customer, only: [:create]
+  before_action :reject_inactive_customer, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -33,5 +33,13 @@ class Public::SessionsController < Devise::SessionsController
     root_path
   end
 
-
+  def reject_inactive_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted
+        flash[:danger] = 'お客様は退会済みです。申し訳ございませんが、別のメールアドレスをお使いください。'
+        redirect_to new_customer_session_path
+      end
+    end
+  end
 end
