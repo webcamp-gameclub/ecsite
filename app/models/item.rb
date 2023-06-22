c75da9c6fa4d755db8d4e212d3abc05fb710d467
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-06-22 06:56:54 +0000

    [Edit]Itemmodel,headerView,ja.yml

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :image, presence: true
-  #validates :genre_id, presence: true
+  validates :genre_id, presence: true
   validates :price, presence: true
   
   # 消費税を含む金額
