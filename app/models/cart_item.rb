d54d0f259dc77d6576c97e56b5760aee7dc06c7f
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-22 08:07:02 +0000

    Update top

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,9 +1,9 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
-  
+
   validates :amount, presence: true
-  
+
   # 商品毎の小計を算出
   def subtotal
     item.tax_price * amount
