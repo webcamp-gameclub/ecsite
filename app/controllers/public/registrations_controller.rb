7800b4a1ea67bbe49943bb3ebb744537553cf598
yo30M <129662479+yo30M@users.noreply.github.com>
2023-06-22 05:23:20 +0000

    Update devise

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -55,6 +55,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
    mypage_path
   end
 
+  def after_update_path_for(resource)
+   mypage_path
+  end
+
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
