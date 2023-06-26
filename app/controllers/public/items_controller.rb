aaccfa428c37bc3688de9f21fa9ad55eb6995a8e
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-06-26 07:18:25 +0000

    [edit]itemcontrollerデータ復元

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.all.page(params[:page]).per(10)
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
+    @genres = Genre.all
     @cart_item = CartItem.new
   end
 end
